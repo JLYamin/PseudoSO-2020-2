58e50745c0ad636a2bd1b6656546e3c3193ffee4
JLYamin <joaol@novaweb.mobi>
2021-05-09 17:12:51 -0300

    Second chance done

--- src/memory.py
+++ src/memory.py
@@ -21,7 +21,7 @@ class Memory:
     def fifo(self):
         print("FIFO", end=" ")
 
-        idx = 0
+        idx = 0                                     # página mais antiga
         page_fault = 0
         pages_array = [None] * self.pages_num
 
@@ -38,7 +38,41 @@ class Memory:
     def second_chance(self):
         print("SC", end=" ")
 
+        oldest_page = 0
         page_fault = 0
+        pages_array = [{'page': None, 'r': 0, 'ref_count': 0}] * self.pages_num
+
+        for ref in self.ref_sequence:                       # roda entre todas as referencias
+            for page in pages_array:                        # for pages_array
+                if (page['ref_count'] != 0):
+                    # se o ref_count !== 0, ref_count -= 1
+                    page['ref_count'] -= 1
+                else:
+                    page['r'] = 0                           # else, r = 0
+
+            found_page = list(
+                filter(lambda x: x['page'] == ref, pages_array))
+            if (len(found_page) != 0):
+                # se já existir, ref_count = 3 e r = 1
+                found_page[0]['ref_count'] = 3
+                found_page[0]['r'] = 1
+            else:
+                while (True):                               # se não existir, faz um while (true)
+                    if (pages_array[oldest_page]['r'] == 0):
+                        pages_array[oldest_page] = {
+                            'page': ref, 'r': 1, 'ref_count': 3}
+                        page_fault += 1
+                        break
+                    else:                                   # se não, segue pra próxima página
+                        oldest_page = (oldest_page + 1) % (self.pages_num)
+                        for page in pages_array:            # decrementa os ref_count
+                            if (page['ref_count'] != 0):
+                                page['ref_count'] -= 1
+                            else:
+                                page['r'] = 0
+
+                # incrementa oldest_page
+                oldest_page = (oldest_page + 1) % (self.pages_num)
 
         print(page_fault)
 
