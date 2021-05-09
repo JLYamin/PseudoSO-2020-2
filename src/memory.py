e245bcb3611633ddc672245e537e400c7c7af0b9
JLYamin <joaol@novaweb.mobi>
2021-05-09 17:56:07 -0300

    LRU done

--- src/memory.py
+++ src/memory.py
@@ -23,7 +23,7 @@ class Memory:
 
         idx = 0                                     # página mais antiga
         page_fault = 0
-        pages_array = [None] * self.pages_num
+        pages_array = [None for i in range(self.pages_num)]
 
         for ref in self.ref_sequence:
             try:
@@ -40,7 +40,8 @@ class Memory:
 
         oldest_page = 0
         page_fault = 0
-        pages_array = [{'page': None, 'r': 0, 'ref_count': 0}] * self.pages_num
+        pages_array = [{'page': None, 'ref_count': 0}
+                       for i in range(self.pages_num)]
 
         for ref in self.ref_sequence:                       # roda entre todas as referencias
             for page in pages_array:                        # for pages_array
@@ -80,5 +81,25 @@ class Memory:
         print("LRU", end=" ")
 
         page_fault = 0
+        # Inicializa ref_count com o maior número de iterações possível para ser o primeiro a ser removido
+        pages_array = [{'page': None, 'ref_count': self.pages_num + 1}
+                       for i in range(self.pages_num)]
+
+        for ref in self.ref_sequence:
+            found_page = list(
+                filter(lambda x: x['page'] == ref, pages_array))
+
+            if (len(found_page) != 0):      # if (pagina ja existe no array)
+                found_page[0]['ref_count'] = 0  # se sim, zera o ref_count dele
+            else:
+                # se não, ordena o page_array pelo maior ref_count e substitui ele pela nova página
+                pages_array.sort(key=lambda x: (x['ref_count']))
+                pages_array[-1]['page'] = ref
+                pages_array[-1]['ref_count'] = 0
+                page_fault += 1
+
+            # incrementa em 1 todos os ref_counts
+            for page in pages_array:
+                page['ref_count'] += 1
 
         print(page_fault)
